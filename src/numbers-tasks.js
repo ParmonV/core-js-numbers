275b9138c2a5d66707787fde1fff6636e4072d91
Parmon Vova <parmonv333@gmail.com>
2024-12-16 23:18:45 +0300

    added tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -111,8 +111,12 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const sumVec = x1 * x2 + y1 * y2;
+  const sumFirst = Math.sqrt(x1 * x1 + y1 * y1);
+  const sumSecond = Math.sqrt(x2 * x2 + y2 * y2);
+  const res = sumFirst * sumSecond;
+  return Math.acos(sumVec / res);
 }
 
 /**
@@ -128,8 +132,14 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(n) {
+  let res;
+  if (n === 0) {
+    res = 0;
+  } else {
+    res = n % 10;
+  }
+  return res;
 }
 
 /**
@@ -143,8 +153,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return String(value);
 }
 
 /**
@@ -160,8 +170,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -181,8 +191,10 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const num2 = 10 ** pow;
+  const num1 = num / num2;
+  return Math.round(num1) * num2;
 }
 
 /**
@@ -268,8 +280,8 @@ function getFibonacciNumber(/* index */) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  return (n * (n + 1)) / 2;
 }
 
 /**
